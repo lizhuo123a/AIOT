0d8ccb892143df558eba92b3578240ad8b06f324
lizhuo <358840393@qq.com>
2020-11-10 14:44:39 +0800

    增加了新文件

--- cases/test_02_product.py
+++ cases/test_02_product.py
@@ -1,7 +1,7 @@
 # ======================
 # -*- coding: utf-8 -*-
 # @author:LiZhuo
-# @time  :2020/10/13 9:42
+# @time  :2020/11/10 14:17
 # @email :358840393@qq.com
 # 今天的你要比昨天的你更优秀！
 # ======================
@@ -9,23 +9,23 @@ import unittest
 import sys
 sys.path.append("..")
 from scripts.handle_log import HandleLogger
-from scripts.handle_request import do_request 
+from scripts.handle_request import do_request
 from scripts.handle_excel import HandleExcel
 from libs.ddt import ddt,data
 from scripts.handle_config import do_config,HandleConfig
 from scripts.constants import TEST_CONFIG_PATH,TESTCASE_PAHT
 get_log = HandleLogger().get_logger()
-cases = HandleExcel(TESTCASE_PAHT,"login").get_cases()
+cases = HandleExcel(TESTCASE_PAHT,"product").get_cases()
 @ddt
 class TestLogin(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        get_log.info("{:=^40s}".format("开始执行login接口测试用例"))
+        get_log.info("{:=^40s}".format("开始执行product接口测试用例"))
 
     @classmethod
     def tearDownClass(cls) -> None:
-        get_log.info("{:=^40s}".format("login接口测试用例执行结束"))
+        get_log.info("{:=^40s}".format("product接口测试用例执行结束"))
 
     @data(*cases)
     def test_login(self,case):
@@ -38,9 +38,6 @@ class TestLogin(unittest.TestCase):
         try:
             self.assertIn(except_result,real_result,msg=ms)
             get_log.info("{}执行通过".format(ms))
-            if res.json()["data"]:
-                datas = {"info":{"Authorization":res.json()["data"]}}
-                HandleConfig(TEST_CONFIG_PATH).write_config(datas,TEST_CONFIG_PATH)
         except Exception as e :
             get_log.error("{}执行失败".format(ms))
             raise e
